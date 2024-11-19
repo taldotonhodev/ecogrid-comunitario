d69770fa0a3000cb5a6c6c6cd4504673e44164f1
MiguelOttoDev <105008843+MiguelOttoDev@users.noreply.github.com>
2024-11-18 22:38:01 -0300

    Alterações na home e nos componentes header e footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.css"; // Import local do CSS específico do Header
+import styles from "./Header.module.css"; // Importando como módulo CSS
 
 function Header() {
   return (
-    <header className="header">
-      <div className="logo">
+    <header className={styles.header}>
+      <div className={styles.logo}>
         <h1>EcoGrid Comunitário</h1> {/* Sugestão: substituir por logo.png */}
       </div>
-      <nav className="nav">
+      <nav className={styles.nav}>
         <Link to="/">Home</Link>
         <Link to="/problem">O Problema</Link>
         <Link to="/solution">A Solução</Link>
